bc3c7cf4ffc86bb75c1171f4a4c7b597ba4b72b3
Michael Gilliland <mjg.py3@gmail.com>
2016-03-05 16:49:39 -0500

    For repeatability, create queue in consumer.
    Also, wait until ready rather than a static 10 seconds

--- link-visit-tracker/src/app.py
+++ link-visit-tracker/src/app.py
@@ -2,16 +2,43 @@
 
 from time import sleep
 import pika
-sleep(10)
+import redis
 
-credentials = pika.PlainCredentials('guest', 'guest')
-parameters = pika.ConnectionParameters('rabbit',
-                                       5672,
-                                       '/',
-                                       credentials)
+def retry(operation, interval=1):
+  while True:
+    try:
+      r = operation()
+      return r
+    except:
+      sleep(interval)
 
-connection = pika.BlockingConnection(parameters)
+redis_client = retry(
+  lambda:
+    redis.StrictRedis(
+      host='redis',
+      port=6379,
+      db=0
+    )
+)
+
+credentials = pika.PlainCredentials(
+  'guest',
+  'guest'
+)
+parameters = pika.ConnectionParameters(
+  'rabbit',
+  5672,
+  '/',
+  credentials
+)
+
+connection = retry(
+  lambda:
+    pika.BlockingConnection(parameters)
+)
 channel = connection.channel()
+
+channel.queue_declare(queue='linkExtracted', durable=True)
 
 for method_frame, properties, body in channel.consume('linkExtracted'):
     channel.basic_ack(method_frame.delivery_tag)
