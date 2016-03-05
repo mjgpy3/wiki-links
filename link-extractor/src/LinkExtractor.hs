1c0d0f521cae60121e75bf6b28b4d331855cdad5
Michael Gilliland <mjg.py3@gmail.com>
2016-03-04 21:34:17 -0500

    Setup post route

--- link-extractor/src/LinkExtractor.hs
+++ link-extractor/src/LinkExtractor.hs
@@ -1,6 +1,11 @@
-import System.Posix.Unistd
+{-# LANGUAGE OverloadedStrings #-}
 
-main = do
-  sleep 1
-  putStrLn "Foobar"
-  main
+import Web.Scotty
+import Data.Monoid (mconcat)
+import Network.CGI
+
+main = scotty 3000 $ do
+  post "/link" $ do
+    liftIO $ putStrLn "called"
+    body <- body
+    html "<h1>Hi there</h1>"
